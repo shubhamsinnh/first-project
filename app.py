4d5e794537b2764363575150cd00c823973b8db8
Shubham Singh <shubhprsl@gmail.com>
2025-02-18 21:41:45 +0530

    Added Priests & Book button

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
 from flask import Flask, render_template
 
+Jobs = [
+    {"name": "Pankaj Jha", 
+     "experience": "4 years", 
+     "age": "40 years", 
+     "availability": True, 
+     "Location": "Chennai"},
+   
+    {"name": "Medhansh Acharya", 
+     "experience": "7 years", 
+     "age": "35 years", 
+     "availability": True, 
+     "Location": "Pune"},
+    
+    {"name": "Govind Kumar Jha", 
+     "experience": "6 years", 
+     "age": "27 years", 
+     "availability": True, 
+     "Location": "New Delhi"},
+    
+    {"name": "Shankar Pandit", 
+     "experience": "9 years", 
+     "age": "39 years", 
+     "availability": True, 
+     "Location": "Bangalore"}
+]
+
+
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=Jobs)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+        app.run(debug=True)
