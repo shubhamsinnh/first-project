14f28312accc451eff9c2c903dbaa6ca2c89aa3e
Shubham Singh <shubhprsl@gmail.com>
2025-03-01 14:49:30 +0530

    database fix | location column & data added
    Updated location in DB

--- app.py
+++ app.py
@@ -37,9 +37,9 @@ class User(db.Model):
 class Priests(db.Model):
     __tablename__ = "priests"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    experience = db.Column(db.String(50), nullable=False)
-    age = db.Column(db.String(20), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    experience = db.Column(db.String(250), nullable=False)
+    age = db.Column(db.String(50), nullable=False)
     location = db.Column(db.String(100), nullable=False)
     availability = db.Column(db.Boolean, default=True)
 
