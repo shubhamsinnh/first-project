fa785ff9742f3835b0952334087c06e3a58a1c88
Shubham Singh <shubhprsl@gmail.com>
2025-03-06 04:29:51 +0530

    Home page redesigned
    Redesigned home page

---
+++ models/user.py
@@ -0,0 +1,7 @@
+from database import db
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)  # Hashed password
